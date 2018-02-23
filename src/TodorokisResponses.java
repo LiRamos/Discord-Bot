d47ae415e358b8d6c6035e1f1979d5daeeed45f6
Lissette Ramos <ramoslissette22@yahoo.com>
2018-02-23 07:08:30 -0500

    Update TodorokisResponses.java

--- src/TodorokisResponses.java
+++ src/TodorokisResponses.java
@@ -6,7 +6,7 @@ public class TodorokisResponses extends ListenerAdapter {
 public void onMessageReceived(MessageReceivedEvent event){
 	String message = event.getMessage().getContent();
 	if (message.startsWith("b!hi")){ //Gets content of a user's message, which must start with this.
-		String name = event.getAuthor().getName(); //gets name of author
+		String name = event.getAuthor().getName(); //gets name of user
 		String response ="Hello" + " " + name + ", " +  "need something?";
 		event.getTextChannel().sendMessage(response).complete();
 		}
